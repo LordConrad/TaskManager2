167a89adc74916ff6c75964f2162ab5376e7237e
Butkevich Siarhei <sbutkevich@scnsoft.com>
2016-06-13 13:59:56 +0300

    hide navbar while logged off

--- TaskManager2/scripts/app/controllers/navbarController.js
+++ TaskManager2/scripts/app/controllers/navbarController.js
@@ -5,19 +5,19 @@
         function ($scope, authService) {
             $scope.isAuth = function() {
                 return authService.authData.isAuth;
-            }
+            };
 
             $scope.userId = function() {
                 return authService.authData.userId;
-            }
-            
+            };
+
             $scope.fullName = function() {
                 return authService.authData.username;
-            }
-            
+            };
+
             $scope.logOut = function() {
                 authService.logOut();
-            }
+            };
         }
     ]);
 })();
