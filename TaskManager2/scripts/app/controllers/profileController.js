a9a495fdf6792ec83ac25b3fffde1882cd73bc99
lordconrad <butkevich.siarhei@gmail.com>
2016-06-30 01:30:54 +0300

    add field editor directive

--- TaskManager2/scripts/app/controllers/profileController.js
+++ TaskManager2/scripts/app/controllers/profileController.js
@@ -4,27 +4,13 @@
         'authService',
         '$location',
         function ($scope, authService, $location) {
-            var backupValues = {};
 
-            $scope.setEditField = function (propertyName) {
-                backupValues[propertyName] = $scope.profileInfo[propertyName].value;
-                $scope.profileInfo[propertyName].isEdit = true;
-            }
-
-            $scope.cancelEditField = function (propertyName) {
-                $scope.profileInfo[propertyName].value = backupValues[propertyName];
-                $scope.profileInfo[propertyName].isEdit = false;
-            }
-
-            $scope.acceptEditField = function(propertyName) {
-                $scope.profileInfo[propertyName].isEdit = false;
-            }
 
             if (authService.authData.isAuth) {
                 $scope.profileInfo = {
                     username: {
-                        value: authService.authData.username,
-                        isEdit: false
+                        title: 'Имя пользователя',
+                        value: authService.authData.username
                     }
                 };
 
---
+++ TaskManager2/scripts/app/directives/fieldEditor.js
@@ -0,0 +1,40 @@
+﻿(function() {
+    angular.module('app').directive('fieldEditor', function() {
+
+        return {
+            restrict: 'A',
+            templateUrl: "scripts/app/views/templates/fieldEditor.html",
+            replace: true,
+            scope: {
+                fieldObject: '=fieldEditor'
+            },
+            link: function(scope, element, attrs) {
+                scope.view = {
+                    editableValue: scope.fieldObject.value,
+                    title: scope.fieldObject.title,
+                    isEditEnable: false
+                }
+
+                scope.enableEdit = function() {
+                    scope.view.isEditEnable = true;
+                    scope.view.editableValue = scope.fieldObject.value;
+                    setTimeout(function() {
+                        element.find('input')[0].focus();
+                    });
+                    
+                }
+
+                scope.disableEdit = function() {
+                    scope.view.isEditEnable = false;
+                }
+
+                scope.save = function() {
+                    scope.fieldObject.value = scope.view.editableValue;
+                    scope.disableEdit();
+                }
+            }
+        }
+    });
+})();
+
+
